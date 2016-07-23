d0cec94fa21d80006c3ff46561e71b6d0586970b
felix <felix@volny.co>
2016-07-23 18:06:14 +0700

    template controls and inheritance

--- app/views.py
+++ app/views.py
@@ -5,6 +5,21 @@ from app import app
 @app.route('/index')
 def index():
   user = {'nickname': 'Phil'}
+  posts = [
+    {
+      'author': {'nickname': 'John'},
+      'body': 'It\'s nice out here today.'
+    },
+    {
+      'author': {'nickname': 'Richard'},
+      'body': 'It\'s indeed a nice day, sir.'
+    }
+  ]
+
+
+
+
   return render_template('index.html',
                           title="Home",
-                          user=user)
+                          user=user,
+                          posts=posts)
