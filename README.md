cded47d672dc3dc8bdb3910d11ba61a1db58b26e
felix <felix@volny.co>
2016-07-25 10:12:17 +0700

    create db

---
+++ README.md
@@ -0,0 +1,1 @@
+Create the db with `chmod a+x create_db.py`, `./create_db.py`
--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # use the config file
 app.config.from_object('config')
-from app import views
+db = SQLAlchemy(app)
+
+from app import views, models
